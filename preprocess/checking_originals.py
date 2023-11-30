dec535aaca01b35fa71a546f335edd983f61406d
cws7777 <cws7777@hotmail.com>
2023-11-30 09:02:08 +0900

    Updating stuffs

--- preprocess/checking_originals.py
+++ preprocess/checking_originals.py
@@ -2,7 +2,7 @@ import json
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--qajson",  help="Required json file of DramaQA", type=str, default="../originals/AnotherMissOh_train_set.json")
+parser.add_argument("--qajson",  help="Required json file of DramaQA", type=str, default="../originals/AnotherMissOhQA_train_set.json")
 parser.add_argument("--output", help="Processed file output file path", type=str, default="../processed/DramaQA_train_set.txt")
 
 args = parser.parse_args()
@@ -17,8 +17,10 @@ with open(args.qajson) as f:
         if shot_level == "shot":
             vid_id = data[i]["vid"]
             q_level = data[i]["q_level_logic"]
-            summary = shot_level + vid_id + str(q_level)
+            summary = shot_level + ', ' + vid_id + ', ' + str(q_level)
             out_file.write(summary)
             out_file.write('\n')
         else:
             pass
+
+print("Done")
