68355fbe4ff4e021ac1169fed4ddda34ea09c0de
cws7777 <cws7777@hotmail.com>
2023-12-08 18:22:42 +0900

    Creating subQA for shots

--- preprocess/format_shot.py
+++ preprocess/format_shot.py
@@ -2,24 +2,53 @@ import json
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--capfile",  help="Required original caption file of DramaQA", type=str, default="../originals/DramaCap_test.json")
-parser.add_argument("--tuplefile",  help="Required txt file of DramaCaption tuples", type=str, default="../processed/DramaCap_test_shotscene_wo_sent.txt")
+parser.add_argument("--subqafile",  help="Required original caption file of DramaQA", type=str, default="../processed/AnotherMissOh_integrated_val.txt")
+parser.add_argument("--output",  help="Required jsonrows file for output file", type=str, default="../processed/subqa_val.json.rows")
 
+args = parser.parse_args()
 
-def load_amr_graph_sent(fpath):
-    entries = {'amrs':[]}
-    with open(fpath) as f:
-        data = f.read()
-    for entry in data.split('\n'):
+output_file = args.output
+subqa_file = args.subqafile
+outfile = open(output_file, 'w')
+
+#def load_amr_graph_sent(fpath):
+    #entries = {'amrs':[]}
+with open(subqa_file) as f:
+    data = f.read()
+total_list = []
+for entry in data.split('\n\n'):
         #sent     = None
-        gstrings = []
-        for line in entry.splitlines():
+        #vid__list = []
+    integ_dict = {}
+    que_list = []
+    ans_list = []
+    vid = ""
+    for line in entry.splitlines():
             #line = line.strip()
+        #print(line)
+        #exit(0)
             #if line.startswith('# ::snt'):
             #    sent = line[len('# ::snt'):].strip()
-            if line.startswith('shot_id:'):
-                gstrings.append( line )
-        if gstrings:
-            #entries['sent'].append(sent)
-            entries['amrs'].append(' '.join(gstrings))
-    return entries['amrs']
+        if line.startswith('shot_id:'):
+            vid = line[line.find(':')+2:]
+            vid = vid.replace(",","")
+
+        if line.startswith('Q'):
+            que_list.append(line[line.find(':')+2:])
+        if line.startswith('1.'):
+            que_list.append(line[line.find('.')+2:])
+        if line.startswith('2.'):
+            que_list.append(line[line.find('.')+2:])
+
+        if line.startswith('A'):
+            ans_list.append(line[line.find(':')+2:])
+
+    #print(vid)
+    #print(que_list)
+    #print(ans_list)
+
+    integ_dict["shot_id"] = vid
+    integ_dict["subq"] = que_list
+    integ_dict["suba"] = ans_list
+    outfile.write(json.dumps(integ_dict))
+    outfile.write('\n')
--- preprocess/integrate_file.py
+++ preprocess/integrate_file.py
@@ -2,20 +2,27 @@ import json
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--jsonrows",  help="Required json.rows file of DramaCaption", type=str, default="../DramaCap_train_shot.json.rows")
-parser.add_argument("--tupletxt",  help="Required txt file of DramaCaption tuples", type=str, default="../processed/DramaCap_train_shotscene_wo_sent.txt")
-parser.add_argument("--output", help="Processed file output file path", type=str, default="./DramaCap_test_ep118_test.txt")
+#parser.add_argument("--jsonrows",  help="Required json.rows file of DramaCaption", type=str, default="../DramaCap_train_shot.json.rows")
+#parser.add_argument("--tupletxt",  help="Required txt file of DramaCaption tuples", type=str, default="../processed/DramaCap_train_shotscene_wo_sent.txt")
+#parser.add_argument("--output", help="Processed file output file path", type=str, default="./DramaCap_test_ep118_test.txt")
+
+parser.add_argument("--jsonrows",  help="Required json.rows file of DramaCaption", type=str, default="../processed/subqa_test.json.rows")
+parser.add_argument("--jsonfile",  help="Required json file of DramaQA", type=str, default="../originals/AnotherMissOhQA_test_set.json")
+parser.add_argument("--output",  help="Required json file of DramaQA", type=str, default="../originals/AnotherMissOhQA_subQA_test_set.json")
+
 
 args = parser.parse_args()
 out_file = open(args.output, 'w')
 
-open_file = open(args.jsonrows, 'r')
-txt_file = open(args.tupletxt, 'r')
-
+jsonrows_file = open(args.jsonrows, 'r')
+json_file = open(args.jsonfile, 'r')
+qa_data = json.load(json_file)
+leng = len(qa_data)
+#print(leng)
 #serials = {'img_id': [], 'sentence': [], 'tuples': []}
 #print('Loading and integrating', fpath)
-count = 0
-
+#count = 0
+'''
 for file_json in open_file:
     data = json.loads(file_json)
     img_id = data['shot_id']
@@ -28,4 +35,57 @@ for file_json in open_file:
     out_file.write('\n')
 out_file.close()
 print("Done integrating")
+'''
+count = 0
+count_json = 0
+count_s = 0
+json_list = []
+for file_json in jsonrows_file:
+    data = json.loads(file_json)
+    json_list.append(data)
+leng_data = len(json_list)
+total_li = []
+for i in range(leng):
+    if qa_data[i]['videoType'] == "shot":
+        vid = qa_data[i]['vid']
+        count = count+1
+        new_dict = {}
+        for j in range(leng_data):
+            sub_li = []
+            shot_id = json_list[j]['shot_id']
+            sub_q = json_list[j]['subq']
+            sub_a = json_list[j]['suba']
+            sub_li.append(sub_q)
+            sub_li.append(sub_a)
 
+            if shot_id == vid:
+                count_s = count_s + 1
+                new_dict = qa_data[i]
+                new_dict['sub_qas'] = sub_li
+                total_li.append(new_dict)
+out_file.write(json.dumps(total_li, indent=4))
+
+'''
+        for file_json in jsonrows_file:
+            count_json = count_json + 1
+            subqa_li = []
+            data = json.loads(file_json)
+            print(data)
+            exit(0)
+            img_id = data["shot_id"]
+            sub_q = data["subq"]
+            sub_a = data["suba"]
+            #new_dict = {}
+            subqa_li.append(sub_q)
+            subqa_li.append(sub_a)
+
+            if qa_data[i]["vid"] == data["shot_id"]:
+                count_s = count_s + 1
+                new_dict = qa_data[i]
+                new_dict['sub_qas'] = subqa_li
+                json_object = json.dumps(new_dict, indent=4)
+                out_file.write(json_object)
+print(count)
+print(count_json)
+print(count_s)
+'''
