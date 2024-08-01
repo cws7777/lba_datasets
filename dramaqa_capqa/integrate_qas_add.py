89f2030ac86c450ea17ccb69cb2d1049a833c4a0
cws7777 <cws7777@hotmail.com>
2024-08-01 20:13:06 +0900

    creating a format of originals for additional qas

--- dramaqa_capqa/integrate_qas_add.py
+++ dramaqa_capqa/integrate_qas_add.py
@@ -1,9 +1,9 @@
 import json
 
 # Paths to the input and output files
-txt_file_path = './additional_qas/AnotherMissOh10_sceneaddQA_20240522.txt'
+txt_file_path = './additional_qas/AnotherMissOh09_sceneaddQA_20240522.txt'
 json_file_path = '../originals/AnotherMissOhQA_train_set.json'
-output_file_path = './AnotherMissOhQA10_updated.json'
+output_file_path = './AnotherMissOhQA09_updated.json'
 
 # Function to parse the text file and extract vid, questions, answers, and question types
 def parse_txt_file(file_path):
@@ -15,7 +15,7 @@ def parse_txt_file(file_path):
     qa_pairs = []
 
     for line in lines:
-        if line.startswith("AnotherMissOh10"):
+        if line.startswith("AnotherMissOh09"):
             if current_vid and qa_pairs:
                 data[current_vid] = qa_pairs
                 qa_pairs = []
